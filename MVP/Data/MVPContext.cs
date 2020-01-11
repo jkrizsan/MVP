d9ea7f71b0c9568215fb861918384e6edd44eacc
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2020-01-11 09:03:42 +0100

    Init Steps

---
+++ MVP/Data/MVPContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVP.Data.Models;
+using System;
+
+namespace MVP.Data
+{
+    public class MVPContext : DbContext
+    {
+        public MVPContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
