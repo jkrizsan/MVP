d25d6a75f358eca8c9f7562f4594b37f7092c7c1
József Krizsán <fysikko@gmail.com>
2022-01-01 13:12:04 +0100

    Replace Initial Data Project functionality with EF Seeds

--- MVP/Data/Migrations/20220101120603_AddSeed.Designer.cs
+++ MVP/Data/Migrations/20220101120603_AddSeed.Designer.cs
@@ -3,14 +3,16 @@ using MVP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVP.Data.Migrations
 {
     [DbContext(typeof(MVPContext))]
-    partial class MVPContextModelSnapshot : ModelSnapshot
+    [Migration("20220101120603_AddSeed")]
+    partial class AddSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MVP/Data/Migrations/20220101120603_AddSeed.cs
@@ -0,0 +1,225 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVP.Data.Migrations
+{
+    public partial class AddSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "Tax" },
+                values: new object[,]
+                {
+                    { 1, "Austria", 20.0 },
+                    { 28, "United Kingdom", 20.0 },
+                    { 27, "Sweden", 25.0 },
+                    { 26, "Spain", 21.0 },
+                    { 25, "Slovenia", 22.0 },
+                    { 24, "Slovakia", 20.0 },
+                    { 23, "Romania", 19.0 },
+                    { 22, "Portugal", 23.0 },
+                    { 21, "Poland", 23.0 },
+                    { 20, "Netherlands", 21.0 },
+                    { 19, "Malta", 18.0 },
+                    { 18, "Luxembourg", 17.0 },
+                    { 16, "Latvia", 21.0 },
+                    { 15, "Italy", 22.0 },
+                    { 17, "Lithuania", 21.0 },
+                    { 13, "Hungary", 27.0 },
+                    { 2, "Belgium", 21.0 },
+                    { 3, "Bulgaria", 20.0 },
+                    { 4, "Cyprus", 19.0 },
+                    { 5, "Czech Republic", 21.0 },
+                    { 14, "Ireland", 23.0 },
+                    { 7, "Denmark", 25.0 },
+                    { 6, "Croatia", 25.0 },
+                    { 9, "Finland", 24.0 },
+                    { 10, "France", 20.0 },
+                    { 11, "Germany", 19.0 },
+                    { 12, "Greece", 24.0 },
+                    { 8, "Estonia", 20.0 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 4, "Pizza", 30.0 },
+                    { 1, "Apple", 100.0 },
+                    { 2, "Banana", 199.0 },
+                    { 3, "Car", 9990.5 },
+                    { 5, "Eggs", 24.0 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
