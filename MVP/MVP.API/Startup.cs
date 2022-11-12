a82a3f2c709275b8445eeb478fa4b139a66156b9
József Krizsán <fysikko@gmail.com>
2022-11-12 10:13:43 +0100

    Add Authenticate option to the Swagger. Improve Postman tests

--- MVP/MVP.API/Startup.cs
+++ MVP/MVP.API/Startup.cs
@@ -71,9 +71,34 @@ namespace MVP.API
                 };
             });
 
-                services.AddSwaggerGen(c =>
+                services.AddSwaggerGen(opt =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MVP", Version = "v1" });
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MVP", Version = "v1" });
+
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
             });
 
             services.AddControllers().AddNewtonsoftJson();
